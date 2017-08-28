b27a65eef08e7ff69d5dfeb77dd7cabf6b115bae
Marty Hernandez Avedon <martyavedon@gmail.com>
2017-08-28 04:15:57 -0400

    first commit

---
+++ CheckWinState.swift
@@ -0,0 +1,123 @@
+//
+//  CheckWinState.swift
+//  ticTacToeHomework
+//
+//  Created by Marty Hernandez Avedon on 08/28/17.
+//  Copyright © 2017 Marty's . All rights reserved.
+//
+
+import Foundation
+
+func checkWinState(player: String) {
+    /* 
+     The function doesn't know about any of the variables inside the game yet. 
+     When we use the function in our game, we tell it the player's name so it can
+     print out the proper messages. Like so --
+     
+     checkWinState(player: player1)
+     */
+    
+    // this function is very long...mostly because i wanted to avoid hardcoding in what a win looks like
+    
+    var checkForwardDiagonals = "" // in a regular Tic-Tac-Toe grid, these are 1, 5, 9
+    var checkBackwardDiagonals = "" // in a regular Tic-Tac-Toe grid, these are 3, 5, 7
+    var checkColumns: [String] = []
+    
+    for outerIndex in 0..<lengthOfGrid {
+        // check rows
+        
+        if internalGrid[outerIndex].joined() == String(repeating: "X", count: widthOfGrid) {
+            print("\(player) wins!") // .joined() gives us a single string made up of everything inside the array
+            ongoingGame = false
+            break
+        }
+        
+        if internalGrid[outerIndex].joined() == String(repeating: "O", count: widthOfGrid) {
+            print("Computer wins!")
+            ongoingGame = false
+            break
+        }
+        
+        // load up our diagnol & column checkers
+        
+        for innerIndex in 0..<widthOfGrid {
+            checkColumns.append(internalGrid[outerIndex][innerIndex])
+            
+            if innerIndex == outerIndex {
+                checkForwardDiagonals += internalGrid[outerIndex][innerIndex]
+            }
+            
+            if innerIndex == widthOfGrid - outerIndex - 1 {
+                checkBackwardDiagonals += internalGrid[outerIndex][innerIndex]
+            }
+            
+        }
+        
+        // check forward diagonals
+        
+        if checkForwardDiagonals == String(repeating: "X", count: widthOfGrid) {
+            print("\(player) wins!")
+            ongoingGame = false
+            break
+        }
+        
+        if checkForwardDiagonals == String(repeating: "O", count: widthOfGrid) {
+            print("Computer wins!")
+            ongoingGame = false
+            break
+        }
+        
+        // check backward diagonals
+        
+        if checkBackwardDiagonals == String(repeating: "X", count: widthOfGrid) {
+            print("\(player) wins!")
+            ongoingGame = false
+            break
+        }
+        
+        if checkBackwardDiagonals == String(repeating: "O", count: widthOfGrid) {
+            print("Computer wins!")
+            ongoingGame = false
+            break
+        }
+        
+        // check columns
+        
+        for currentColumn in 0..<widthOfGrid {
+            var column = ""
+            
+            for index in stride(from: currentColumn, to: checkColumns.count, by: widthOfGrid) {
+                column += checkColumns[index]
+            }
+            
+            /*
+             We made checkColumns one long array of strings so we could use stride on it.
+             Stride lets us go through the array by skipping over some elements 
+             and by starting in different places on each trip through the loop.
+             
+             On our first trip, we start on index 0, and stride by 3's, so we go down 1, 4, 7.
+             Then we go to index 1, and stride down by 3's -- 2, 5, 8.
+             Then we hit our last index, 2, and stride down by 3's -- 3, 6, 9.
+             
+             There is a method called .flatMap() that we could have used to make 
+             our three-array grid into one long array. We didn't use it here because
+             .flatMap()'s use can get kind of confusing.
+             */
+            
+            if column == String(repeating: "O", count: widthOfGrid) {
+                print("Computer wins!")
+                ongoingGame = false
+                break
+            }
+            
+            if column == String(repeating: "X", count: widthOfGrid) {
+                print("\(player) wins!")
+                ongoingGame = false
+                break
+            }
+            
+        }
+    }
+}
+
+// this function contains a lot of repetitive code. how could we make it smaller?
--- ticTacToeHomework/main.swift
+++ ticTacToeHomework/main.swift
@@ -6,7 +6,193 @@
 //  Copyright © 2017 Marty's . All rights reserved.
 //
 
-import Foundation
+import Foundation // importing Foundation lets you use all sorts of built-in functions and classes
 
-print("Hello, World!")
+var ongoingGame: Bool
 
+let prompt = ">> "
+var player1: String = "Anonymous" // this gives the player a default name if they don't type one in later
+
+let lengthOfGrid = 3 // we make the length and width variable in case we want to play bigger or smaller games
+let widthOfGrid = 3
+let gridSize = lengthOfGrid * widthOfGrid
+
+var internalGrid: [[String]] // our game board. it's not initialized yet because the game hasn't started
+var innerArray: [String] // this will be used and resused to fill our grid with rows
+
+var round: Int
+
+
+print("*--- _Tic_|_Tac_|_Toe_ ---* \n")
+print("Welcome! What is your name?", terminator: " ") // personalizing the game is always a nice touch
+
+while true { // we want our game to keep running and running, until we break this loop and quit
+    print("If you want to stop playing, type 'quit' to exit.", prompt, separator: "\n")
+    
+    let playersResponse = readLine()!
+    
+    if playersResponse.lowercased() == "quit" {
+        break
+    }
+    
+    /*
+     using the String method .lowercased() above makes it easier to match the 
+     user's answer with our string, "quit".
+     
+     when we take in user input, we should make it all the same case --
+     otherwise we'll have to check if the user typed quit, Quit, QUIT, etc. 
+     
+     If we used something called regular expressions, we wouldn't have to use .lowercased(),
+     but regular expressions are a much more complicated topic than String methods!
+     
+     See https://code.tutsplus.com/tutorials/swift-and-regular-expressions-swift--cms-26626
+     */
+    
+    round = 0 // we start counting rounds once we know the player hasn't quit
+    
+    if playersResponse != "" {
+        player1 = playersResponse // if we get no answer the first time the player plays, the default value is "Anonymous". If they win or lose a game and restart, they can choose to keep their current name or change it
+    }
+    
+    print("\nHi, \(player1)! Let's get started!")
+    print("Drawing the board...", "\n")
+    
+    sleep(1) // the pause makes it seem like drawing a grid is really intense
+    
+    print("Try to beat me! Make a straight line of X's inside the grid and win! \n")
+    
+    internalGrid = [] // we reset the grid to empty
+    innerArray = [] // this is a container for the rows inside our grid. It gets reset on each trip through the loop
+    
+    for number in 1...gridSize {
+        innerArray.append(String(number)) // on each trip through a row, we append the number to our inner array
+        
+        if number % widthOfGrid > 0 { // These are our first & middle columns
+            print("_\(number)_|", terminator: "")
+        } else { // This is our last column -- 3, 6, 9, if we're doing the usual Tic-Tac-Toe
+            print("_\(number)_\n")
+            
+            internalGrid.append(innerArray) // once we reach the last column in a row, we append the innerArray we just filled up...
+            innerArray = [] //  and then empty the innerArray for the next trip through a row
+        }
+        
+        // Note: We fill up our grid while printing another grid just for show. Doing it this way means we don't have to write two loops -- one to fill the grid and one to display it
+    }
+    
+    ongoingGame = true // we have a player and a grid! we can now start playing!
+    
+    while ongoingGame {
+        round += 1
+        
+        var promptPlayer = true
+        
+        while promptPlayer {
+            print("\nType a number to indicate where on the grid you want your X to go!", prompt, separator: "\n")
+            
+            let playersChoice = readLine()!
+            
+            if let coordinate = Int(playersChoice) {
+                guard coordinate < 10 else { // guard statements help us handle errors in user input before we get too far into the loop
+                    print("That number is too big!") // it's a good idea to give your user advice on what wrong when they make an error
+                    continue // go to top of the promptPlayer loop and start again
+                }
+                
+                guard coordinate > 0 else {
+                    print("That number is too small!")
+                    continue
+                }
+                
+                let innerIndex: Int
+                let outerIndex: Int
+                
+                // these calculations are only true for square grids...if you can figure out the math for any kind of grid, congrats.
+                
+                if coordinate % widthOfGrid != 0 {
+                    innerIndex = (coordinate % widthOfGrid) - 1
+                    outerIndex =  coordinate / (gridSize / widthOfGrid)
+                } else {
+                    innerIndex = widthOfGrid - 1
+                    outerIndex = (coordinate / (gridSize / lengthOfGrid)) - 1
+                }
+                
+                if internalGrid[outerIndex][innerIndex] == "X" || internalGrid[outerIndex][innerIndex] == "O" {
+                    print("Oops, that spot is already filled!")
+                    continue
+                }
+                
+                internalGrid[outerIndex][innerIndex] = "X"
+                
+                print("")
+                for outerIndex in 0..<lengthOfGrid {
+                    for innerIndex in 0..<widthOfGrid {
+                        if innerIndex != (widthOfGrid - 1) {
+                            print("_\(internalGrid[outerIndex][innerIndex])_", terminator: "|")
+                        } else {
+                            print("_\(internalGrid[outerIndex][innerIndex])_")
+                        }
+                    }
+                }
+                print("")
+                
+                promptPlayer = false
+            } else {
+                print("I need a digit between 1 and \(gridSize)!") // this else statement only runs if the user doesn't type a number
+            }
+        }
+        
+        checkWinState(player: player1) // checkWinState is a long function put inside its own file. I tried doing this homework without using functions in case you all haven't covered them yet, but it got pretty brutal since I'm also trying to avoid hardcoding the win/lose positions
+        
+        if ongoingGame == false {
+            break // oddly, we can have the checkWinState function set ongoingGame to false...but the game will keep running...so we need this extra break
+        }
+        
+        print("\nNow my turn!\n")
+        
+        var computerPicks = true
+        
+        while computerPicks {
+            
+            let outerRandom = Int(arc4random_uniform(3)) // as dave posted in code snippets, we can use this built in function to randomize our games. But we need to make the result an Int, because otherwise we get an Int32, and we can't use that as an index
+            let innerRandom = Int(arc4random_uniform(3))
+            let computersChoice = internalGrid[outerRandom][innerRandom]
+            
+            if computersChoice == "X" || computersChoice == "O" {
+                continue
+            } else {
+                internalGrid[outerRandom][innerRandom] = "O"
+                
+                for outerIndex in 0..<lengthOfGrid {
+                    for innerIndex in 0..<widthOfGrid {
+                        if innerIndex != (widthOfGrid - 1) {
+                            print("_\(internalGrid[outerIndex][innerIndex])_", terminator: "|")
+                        } else {
+                            print("_\(internalGrid[outerIndex][innerIndex])_")
+                        }
+                    }
+                }
+                print("")
+                
+                computerPicks = false
+            }
+        }
+        
+        checkWinState(player: player1)
+        
+        if round == 9 { // it would be nice if we had some logic for checking to see if the game is unwinnable...but this will do for now
+            print("Looks like a draw!")
+            break
+        }
+    }
+    
+    // this stuff is only displayed once the game is over
+    
+    print("\nDo you want to try again?", prompt, separator: "\n")
+    let playAgain = readLine()!
+    
+    if playAgain.lowercased() == "yes" {
+        ongoingGame = true
+        print("\nIf you want to change your name, type something new now. If not, just hit enter.", terminator: " ")
+    } else {
+        break
+    }
+}
