07a9fd63437c853d177f843872da5adf45ec450f
Marty Hernandez Avedon <martyavedon@gmail.com>
2017-08-27 20:08:43 -0400

    Initial Commit

---
+++ ticTacToeHomework/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ticTacToeHomework
+//
+//  Created by Marty Hernandez Avedon on 08/27/17.
+//  Copyright © 2017 Marty's . All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
